f108817a030e594bb947dff309759f42a0f38bec
stephaniecoleman <sproutwingsandfly@gmail.com>
2014-09-22 15:13:49 -0400

    Remove a couple methods that were too complicated.

--- imdb_scraper.rb
+++ imdb_scraper.rb
@@ -2,7 +2,7 @@ require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-class ImdbScraper
+class IMDbScraper
 
 	def initialize(site)
 		@site = site
@@ -15,9 +15,7 @@ class ImdbScraper
 		Director: #{get_director}\n
 		Stars: #{get_stars}\n
 		Storyline: #{get_storyline}\n
-		Rating: #{get_mpaa_rating}\n
-		Budget: #{get_budget}\n
-		Gross: #{get_gross}"
+		Rating: #{get_mpaa_rating}\n"
 	end
 
 	def get_movie_title
@@ -25,43 +23,26 @@ class ImdbScraper
 	end
 
 	def get_director
-		director = @imdb_nokogiri.css('div.txt-block').first.children[3].text
+		director = @imdb_nokogiri.css('div[itemprop=director]').children[3].text
 	end
 
 	def get_stars
-		stars = @imdb_nokogiri.css('div.txt-block')[2].css('a span')
-		stars_array = stars.children.collect do |star| 
-			star.text
-		end
+		stars = @imdb_nokogiri.css('div[itemprop=actors] a span')
+		stars_array = stars.collect do |star| 
+									 	star.text
+									end
+		stars_array.join(", ")
 	end
 
 	def get_storyline
-		storyline = @imdb_nokogiri.css('div[itemprop=description]').children[1].children[0].text.strip
+		storyline = @imdb_nokogiri.css('div[itemprop=description] p').children.first.text.strip
 	end
 
 	def get_mpaa_rating
 		mpaa_rating = @imdb_nokogiri.css('span[itemprop=contentRating]').text
 	end
 
-	def get_budget
-		budget_match = @imdb_nokogiri.css('div.txt-block').select{|div| div.text.match(/Budget/)}
-		if budget_match.length > 0
-			budget_match.first.children[2].text.strip
-		else
-			"No budget listed"
-		end
-	end
-
-	def get_gross
-		gross_match = @imdb_nokogiri.css('div.txt-block').select{|div| div.text.match(/Gross/)}
-		if gross_match.length > 0 
-			gross_match.first.children[2].text.strip
-		else
-			"No gross listed"
-		end
-	end
-
 end
 
-scraper = ImdbScraper.new('http://www.imdb.com/title/tt1291150/?ref_=hm_cht_t4')
-puts scraper.get_all_info
+scraper = IMDbScraper.new('http://www.imdb.com/title/tt1291150/?ref_=hm_cht_t4')
+puts scraper.get_storyline
